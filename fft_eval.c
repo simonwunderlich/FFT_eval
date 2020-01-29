simonwunderlich/FFT_eval

commit 68201c1ccd1246cf1cd51cb19092aaed000500d0

fft_eval: add support for ath11k
The patch is originally takesn from
-> https://source.codeaurora.org/quic/qsdk/oss/system/feeds/wlan-open-fft/plain/fft_eval/patches/0001-fft_eval-Add-support-for-ath11k-sample-platform.patch

Although the code is near identical to that of ath10k it would result in
pretty messy code with lots of if/then/else blocks.

Signed-off-by: John Crispin <john@phrozen.org>

fft_eval.c
@@ -31,6 +31,7 @@
 #if defined(__APPLE__)
   #include <libkern/OSByteOrder.h>
   #define CONVERT_BE16(val)	val = OSSwapBigToHostInt16(val)
+  #define CONVERT_BE32(val)	val = OSSwapBigToHostInt32(val)
   #define CONVERT_BE64(val)	val = OSSwapBigToHostInt64(val)
 #elif defined(_WIN32)
   #define __USE_MINGW_ANSI_STDIO 1
@@ -50,6 +51,7 @@
     #include <endian.h>
   #endif	/* __FreeBSD__ */
   #define CONVERT_BE16(val)	val = be16toh(val)
+  #define CONVERT_BE32(val)	val = be32toh(val)
   #define CONVERT_BE64(val)	val = be64toh(val)
 #endif
 #include <errno.h>
@@ -212,6 +214,32 @@ int fft_eval_init(char *fname)
 
 			handled = 1;
 			break;
+		case ATH_FFT_SAMPLE_ATH11K:
+			if (sample_len < sizeof(result->sample.ath11k.header)) {
+				fprintf(stderr, "wrong sample length (have %zd, expected at least %zd)\n",
+					sample_len, sizeof(result->sample.ath11k.header));
+				break;
+			}
+
+			bins = sample_len - sizeof(result->sample.ath11k.header);
+
+			if (bins != 32 &&
+			    bins != 64 &&
+			    bins != 128 &&
+			    bins != 256) {
+				fprintf(stderr, "invalid bin length %d\n", bins);
+				break;
+			}
+
+			CONVERT_BE16(result->sample.ath11k.header.freq1);
+			CONVERT_BE16(result->sample.ath11k.header.freq2);
+			CONVERT_BE16(result->sample.ath11k.header.max_magnitude);
+			CONVERT_BE16(result->sample.ath11k.header.rssi);
+			CONVERT_BE32(result->sample.ath11k.header.tsf);
+			CONVERT_BE32(result->sample.ath11k.header.noise);
+
+			handled = 1;
+			break;
 		default:
 			fprintf(stderr, "unknown sample type (%d)\n", tlv->type);
 			break;
@@ -279,4 +307,15 @@ void fft_eval_usage(const char *prog)
 	fprintf(stderr, "\n");
 	fprintf(stderr, "(NOTE: maybe debugfs must be mounted first: mount -t debugfs none /sys/kernel/debug/ )\n");
 	fprintf(stderr, "\n");
+	fprintf(stderr, "For ath11k based cards, use:\n");
+	fprintf(stderr, "ip link set dev wlan0 up\n");
+	fprintf(stderr, "echo background > /sys/kernel/debug/ieee80211/phy0/ath11k/spectral_scan_ctl\n");
+	fprintf(stderr, "echo trigger > /sys/kernel/debug/ieee80211/phy0/ath11k/spectral_scan_ctl\n");
+	fprintf(stderr, "iw dev wlan0 scan\n");
+	fprintf(stderr, "echo disable > /sys/kernel/debug/ieee80211/phy0/ath11k/spectral_scan_ctl\n");
+	fprintf(stderr, "cat /sys/kernel/debug/ieee80211/phy0/ath11k/spectral_scan0 > samples\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "(NOTE: maybe debugfs must be mounted first: mount -t debugfs none /sys/kernel/debug/ )\n");
+	fprintf(stderr, "\n");
+
 }

fft_eval.h
@@ -20,6 +20,7 @@ enum ath_fft_sample_type {
         ATH_FFT_SAMPLE_HT20 = 1,
         ATH_FFT_SAMPLE_HT20_40 = 2,
 	ATH_FFT_SAMPLE_ATH10K = 3,
+	ATH_FFT_SAMPLE_ATH11K = 4,
 };
 
 enum nl80211_channel_type {
@@ -116,6 +117,27 @@ struct fft_sample_ath10k {
 	u8 data[0];
 } __attribute__((packed));
 
+/*
+ * ath11k spectral sample definition
+ */
+
+#define SPECTRAL_ATH11K_MAX_NUM_BINS            256
+
+struct fft_sample_ath11k {
+	struct fft_sample_tlv tlv;
+	u8 chan_width_mhz;
+	s8 max_index;
+	u8 max_exp;
+	uint16_t freq1;
+	uint16_t freq2;
+	uint16_t max_magnitude;
+	uint16_t rssi;
+	uint32_t tsf;
+	int32_t noise;
+
+	u8 data[0];
+} __attribute__((packed));
+
 #if defined(__WIN32__)
 #pragma pack(pop)
 #endif
@@ -130,6 +152,10 @@ struct scanresult {
 			struct fft_sample_ath10k header;
 			u8 data[SPECTRAL_ATH10K_MAX_NUM_BINS];
 		} ath10k;
+		struct {
+			struct fft_sample_ath11k header;
+			u8 data[SPECTRAL_ATH11K_MAX_NUM_BINS];
+		} ath11k;
 	} sample;
 	struct scanresult *next;
 };

fft_eval_json.c
@@ -207,7 +207,45 @@ static int print_values(void)
 
 			}
 			break;
+		case ATH_FFT_SAMPLE_ATH11K:
+			{
+				int datamax = 0, datamin = 65536;
+				int datasquaresum = 0;
+				int i, bins;
+				printf("\n{ \"tsf\": %08d, \"central_freq\": %d, \"rssi\": %d, \"noise\": %d, \"data\": [ ", result->sample.ath11k.header.tsf, result->sample.ath11k.header.freq1,
+				       result->sample.ath11k.header.rssi, result->sample.ath11k.header.noise);
 
+				bins = result->sample.tlv.length - (sizeof(result->sample.ath11k.header) - sizeof(result->sample.ath11k.header.tlv));
+
+				for (i = 0; i < bins; i++) {
+					int data;
+
+					data = result->sample.ath11k.data[i];
+					data *= data;
+					datasquaresum += data;
+					if (data > datamax)
+						datamax = data;
+					if (data < datamin)
+						datamin = data;
+				}
+
+				for (i = 0; i < bins; i++) {
+					float freq;
+					int data;
+					float signal;
+					freq = result->sample.ath11k.header.freq1 - (result->sample.ath11k.header.chan_width_mhz) / 2 + (result->sample.ath11k.header.chan_width_mhz * (i + 0.5) / bins);
+
+					data = result->sample.ath11k.data[i];
+					if (data == 0)
+						data = 1;
+					signal = result->sample.ath11k.header.noise + result->sample.ath11k.header.rssi + 20 * log10f(data) - log10f(datasquaresum) * 10;
+					printf("[ %f, %f ]", freq, signal);
+					if (i < bins - 1)
+						printf(", ");
+
+				}
+			}
+			break;
 		}
 
 		printf(" ] }");

fft_eval_sdl.c
@@ -416,6 +416,55 @@ static int draw_sample_ath10k(Uint32 *pixels, struct scanresult *result,
 	return 0;
 }
 
+static int draw_sample_ath11k(Uint32 *pixels, struct scanresult *result,
+			      float startfreq, int highlight)
+{
+	int datamax = 0, datamin = 65536;
+	int datasquaresum = 0;
+	int i, bins;
+
+	bins = result->sample.tlv.length -
+	       (sizeof(result->sample.ath11k.header) -
+		sizeof(result->sample.ath11k.header.tlv));
+
+	for (i = 0; i < bins; i++) {
+		int data;
+
+		data = (result->sample.ath11k.data[i] << result->sample.ath11k.header.max_exp);
+		data *= data;
+		datasquaresum += data;
+		if (data > datamax) datamax = data;
+		if (data < datamin) datamin = data;
+	}
+
+	if (highlight) {
+		/* prints some statistical data about the currently selected
+		 * data sample and auxiliary data. */
+		printf("result: freq %04d/%04d (width %d MHz), %d bins, rssi %04d, noise %04d, max_magnitude %04d max_index %03d max_exp %03d tsf %08d | ",
+		       result->sample.ath11k.header.freq1, result->sample.ath11k.header.freq1,
+		       result->sample.ath11k.header.chan_width_mhz,
+		       bins, result->sample.ath11k.header.rssi,
+		       result->sample.ath11k.header.noise, result->sample.ath11k.header.max_magnitude,
+		       result->sample.ath11k.header.max_index, result->sample.ath11k.header.max_exp,
+		       result->sample.ath11k.header.tsf);
+		printf("datamax = %d, datamin = %d, datasquaresum = %d\n", datamax, datamin, datasquaresum);
+	}
+
+	for (i = 0; i < bins; i++) {
+		float freq;
+		int data;
+		freq = result->sample.ath11k.header.freq1 -
+				(result->sample.ath11k.header.chan_width_mhz ) / 2 +
+				(result->sample.ath11k.header.chan_width_mhz * (i + 0.5) / bins);
+
+		data = result->sample.ath11k.data[i] << result->sample.ath11k.header.max_exp;
+		plot_datapoint(pixels, freq, startfreq, result->sample.ath11k.header.noise,
+			       result->sample.ath11k.header.rssi, data, datasquaresum,
+			       highlight);
+	}
+
+	return 0;
+}
 
 /*
  * draw_picture - draws the current screen.
@@ -491,6 +540,12 @@ static int draw_picture(int highlight, int startfreq)
 
 			draw_sample_ath10k(pixels, result, startfreq, rnum == highlight);
 			/* TODO */
+			break;
+		case ATH_FFT_SAMPLE_ATH11K:
+			if (rnum == highlight)
+				highlight_freq = result->sample.ath11k.header.freq1;
+
+			draw_sample_ath11k(pixels, result, startfreq, rnum == highlight);
 			break;
 		}
 		rnum++;

commit b5fb56d8dbb4a2484edbad17d91a89cc4f7ac89b

fft_eval: Add missing CONVERT_BE32 defines
Fixes: 68201c1ccd12 ("fft_eval: add support for ath11k")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

fft_eval.c
@@ -37,9 +37,11 @@
   #define __USE_MINGW_ANSI_STDIO 1
   #if __BYTE_ORDER == __LITTLE_ENDIAN
     #define CONVERT_BE16(val)	val = _byteswap_ushort(val)
+    #define CONVERT_BE32(val)	val = _byteswap_ulong(val)
     #define CONVERT_BE64(val)	val = _byteswap_uint64(val)
   #elif __BYTE_ORDER == __BIG_ENDIAN
     #define CONVERT_BE16(val)
+    #define CONVERT_BE32(val)
     #define CONVERT_BE64(val)
   #else
     #error Endianess undefined